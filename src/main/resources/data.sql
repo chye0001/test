9eca2053d28719f02c6ec3c3e046c93df64fc959
Chris <chye0001@stud.kea.dk>
2025-01-11 13:06:30 +0100

    created data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Nordic Star', 'TYPE_A');
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Ocean Breeze', 'TYPE_B');
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Wind Rider', 'TYPE_C');
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Sea Explorer', 'TYPE_A');
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Coastal Runner', 'TYPE_B');
+INSERT INTO Sejlbåd (name, båd_type) VALUES ('Harbor Spirit', 'TYPE_C');
