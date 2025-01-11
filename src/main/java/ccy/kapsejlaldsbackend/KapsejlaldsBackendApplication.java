8bf6656a8d4d43a3fa81534365a8dce689b47c08
Chris <chye0001@stud.kea.dk>
2025-01-11 11:27:15 +0100

    initial commit

---
+++ src/main/java/ccy/kapsejlaldsbackend/KapsejlaldsBackendApplication.java
@@ -0,0 +1,13 @@
+package ccy.kapsejlaldsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapsejlaldsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapsejlaldsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ccy/kapsejlaldsbackend/KapsejlaldsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ccy.kapsejlaldsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapsejlaldsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
